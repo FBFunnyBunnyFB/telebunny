5c26e10be65ba6ee184c504383f6f410bc9f9d00
Funny Bunny <fbfunnybunnyfbua@gmail.com>
2023-03-27 23:33:57 +0300

    test(https): create first tests

---
+++ test/https.test.ts
@@ -0,0 +1,39 @@
+import { strict as assert } from "node:assert";
+import { describe, it } from "node:test";
+
+import { HTTPSRequest, HTTPSResponse } from "../src/https";
+
+describe("HTTPS GET Request", async () => {
+    it("should have \"code\" field in response", async () => {
+        const response: HTTPSResponse = await new HTTPSRequest("https://www.google.com/").get();
+        assert.deepStrictEqual(response.hasOwnProperty('code'), true);
+    });
+    it("should have \"headers\" field in response", async () => {
+        const response: HTTPSResponse = await new HTTPSRequest("https://www.google.com/").get();
+        assert.deepStrictEqual(response.hasOwnProperty('headers'), true);
+    });
+    it("should have \"body\" field in response", async () => {
+        const response: HTTPSResponse = await new HTTPSRequest("https://www.google.com/").get();
+        assert.deepStrictEqual(response.hasOwnProperty('body'), true);
+    });
+    it("should have \"json\" field in response", async () => {
+        const response: HTTPSResponse = await new HTTPSRequest("https://www.google.com/").get();
+        assert.deepStrictEqual(response.hasOwnProperty('json'), true);
+    });
+    it("https://www.google.com/ should return 200", async () => {
+        const response: HTTPSResponse = await new HTTPSRequest("https://www.google.com/").get();
+        assert.deepStrictEqual(response['code'], 200);
+    });
+    it("https://www.google.com/404 should return 404", async () => {
+        const response: HTTPSResponse = await new HTTPSRequest("https://www.google.com/404").get();
+        assert.deepStrictEqual(response['code'], 404);
+    });
+    it("https://bad_host.com/ should return error", async () => {
+        try {
+            const response: HTTPSResponse = await new HTTPSRequest("https://bad_host.com/").get();
+        } catch (error) {
+            assert.deepStrictEqual(error['errno'], -71);
+            assert.deepStrictEqual(error['code'], "EPROTO");
+        }
+    });
+})
